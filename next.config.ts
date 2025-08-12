1ae39b064e4077f887a02329e35a2389df70d03c
Adeyemi Gabriel <145411012+yehmight@users.noreply.github.com>
2025-08-13 00:41:30 +0100

    Update next.config.ts

--- next.config.ts
+++ next.config.ts
@@ -4,9 +4,22 @@ const nextConfig: NextConfig = {
   images: {
     domains: ["res.cloudinary.com"],
   },
+  eslint: {
+    ignoreDuringBuilds: true, // This disables ESLint errors failing your production build
+  },
 };
 
 export default nextConfig;
+
+// import type { NextConfig } from "next";
+
+// const nextConfig: NextConfig = {
+//   images: {
+//     domains: ["res.cloudinary.com"],
+//   },
+// };
+
+// export default nextConfig;
 
 
 
